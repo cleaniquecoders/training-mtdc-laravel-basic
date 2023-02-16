3cdb64313cc6ff787a24aa2a751974ac882bae97
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-02-16 12:20:02 +0800

    Fix header

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,15 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{-- {{ __('User Management') }} --}}
-            @lang('User Management')
-
-            <a
-                href="{{ route('users.create') }}"
+        <div class="flex w-full justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                @lang('User Management')
+            </h2>
+            <a href="{{ route('users.create') }}"
                 class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                 @lang('New User')
             </a>
-        </h2>
+        </div>
     </x-slot>
 
     <div>
@@ -23,7 +22,7 @@
                     <th class="border p-4">E-mail</th>
                     <th class="border p-4">Actions</th>
                 </tr>
-                @foreach($users as $user)
+                @foreach ($users as $user)
                     <tr>
                         <td class="border p-4">{{ $user->name }}</td>
                         <td class="border p-4">{{ $user->email }}</td>
